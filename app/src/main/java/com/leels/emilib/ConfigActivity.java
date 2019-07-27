e2bbc35e43f7a970e632d1e03240f01cfafc9416
Cooper <davidontech@gmail.com>
2019-07-27 23:03:54 +1200

    Continue work on UI

---
+++ app/src/main/java/com/leels/emilib/ConfigActivity.java
@@ -0,0 +1,37 @@
+package com.leels.emilib;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.config);
+
+        Button bt = (Button) findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String addr = ((EditText) findViewById(R.id.editTextAddr)).getText().toString();
+                String msg = ((EditText) findViewById(R.id.editTextMsg)).getText().toString();
+                String cmd = ((EditText) findViewById(R.id.editTextCmd)).getText().toString();
+
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("addr",addr);
+                returnIntent.putExtra("msg",msg);
+                returnIntent.putExtra("cmd",cmd);
+                setResult(Activity.RESULT_OK,returnIntent);
+                finish();
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/leels/emilib/MainActivity.java
+++ app/src/main/java/com/leels/emilib/MainActivity.java
@@ -1,23 +1,40 @@
 package com.leels.emilib;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.HashMap;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    HashMap<Integer, EmiMsg> msgMap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        Button bt = (Button) findViewById(R.id.button);
+        msgMap = new HashMap<>();
 
-        bt.setOnClickListener(new View.OnClickListener() {
+        Button bt1 = (Button) findViewById(R.id.button1);
+
+        bt1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent;
+                intent = new Intent(MainActivity.this, ConfigActivity.class);
+                startActivityForResult(intent, 0);
+
+                return false;
+            }
+        });
+
+        bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EmiMsg msg = new EmiMsg("127.0.0.1", 1, 2, null, 0);
@@ -25,5 +42,20 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("ttt", "onClick: " + Integer.toString(ret));
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+
+                String addr = data.getStringExtra("addr");
+                int msgnum = Integer.parseInt(data.getStringExtra("msg"));
+                int cmd = Integer.parseInt(data.getStringExtra("cmd"));
+
+                EmiMsg msg = new EmiMsg(addr, msgnum, cmd, null, 0);
+            }
+        }
     }
 }
